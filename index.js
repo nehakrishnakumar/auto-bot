339c1cf8d9f2a55312bcdb6ade1c521d61c59b22
nehakrishnakumar <nehakk.2014@gmail.com>
2020-04-01 13:52:00 -0400

    Create gitignore, regenerate token, and implement config.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'); //require() connects to the Discord api
 const bot = new Discord.Client(); //inherited from Discord library
 
-const token = 'Njk0OTE5MTA0NjkxMzA2NTI2.XoSoKg.B8QRnFv7hkU34LznPsJc7XGyeTw';
+const token = require('./config.json');
 
 bot.on('ready', () =>{  
 	console.log('This bot is online!');
