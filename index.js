8d4aaf49a95f5e1d3cfcf0d93d28ebe904f33e56
nehakrishnakumar <nehakk.2014@gmail.com>
2020-04-17 17:18:08 -0400

    Got bot online, sent first example message and tested on server

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ bot.on('ready', () =>{
 	console.log('This bot is online!');
 })
 
+bot.on('message', msg=>{
+	//if (msg.author.bot) return;
+	//else if (msg.channel.type === 'dm') return;
+	
+	//TEMPORARY FOR TESTING
+	if (msg.content === "ur an idiot") {
+		//msg.reply ("Hey! I'm still in development so don't talk to me right now!")
+		msg.reply ("I would really, really insult you but this is on a public repository so I don't want my future boss to get mad");
+		msg.reply ("We can still definitely throw hands though");
+	}
+})
+
 bot.login(token);
