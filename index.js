5c0b615409adadea22c1bbe30fa9569de1f3bd47
nehakrishnakumar <nehakk.2014@gmail.com>
2020-04-17 16:53:18 -0400

    Made small change that made token valid, bot officially online!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'); //require() connects to the Discord api
 const bot = new Discord.Client(); //inherited from Discord library
 
-const token = require('./config.json');
+const { token } = require('./config.json');
 
 bot.on('ready', () =>{  
 	console.log('This bot is online!');
