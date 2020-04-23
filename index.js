38fab5ee982629edb5b8a367dc6f4fc62571ed9e
nehakrishnakumar <nehakk.2014@gmail.com>
2020-04-23 11:02:12 -0400

    Added more cases for the switch statement and did more experimenting, default didn't work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js'); //require() connects to the Discord api
 const bot = new Discord.Client(); //inherited from Discord library
 
 const { token } = require('./config.json');
+const prefix = "?";
 
 bot.on('ready', () =>{  
 	console.log('This bot is online!');
@@ -10,11 +11,22 @@ bot.on('ready', () =>{
 bot.on('message', msg=>{
 	//if (msg.author.bot) return;
 	//else if (msg.channel.type === 'dm') return;
-	
+	let args = msg.content.substring(prefix.length).split(" ");
 	//TEMPORARY FOR TESTING
-	if (msg.content === "Hi BanMeBot!") {
-		msg.reply ("Hey! I'm still in development so don't talk to me right now!")
+	switch(args[0]) {
+		case 'Heyyo':
+			msg.reply('Hey ya! I am still in development right now so talk to me later!');
+			break;
+		case 'Hi':
+			msg.reply ("Hi there. I'm still being developed so talk to me later!");
+			break;
+		/*default:
+			msg.reply("Hi! I don't know what you're saying. Reply with ?Heyyo or ?Hi if you want a greeting!");
+			break;
+			*/
+
 	}
+
 })
 
 bot.login(token);
