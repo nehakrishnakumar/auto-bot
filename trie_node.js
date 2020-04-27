26739daf6f04a16bf64335e3e7559b1f3ff198e4
nehakrishnakumar <nehakk.2014@gmail.com>
2020-04-27 09:07:16 -0400

    Made basic trie_node class

--- trie_node.js
+++ trie_node.js
@@ -6,4 +6,28 @@ class TrieNode {
 		children = c;
 		endOfWord = eOfWord;
 	}
+
+	get children() {
+		return this.children;
+	}
+	get endOfWord() {
+		return this.endOfWord;
+	}
+	set children(c) {
+		this.children = c;
+	}
+	set endOfWord(e) {
+		this.endOfWord = e;
+	}
+	describe() {
+		if (children && !endOfWord) {
+			return "This node has children and is not the end of the word";
+		}
+		else if (endOfWord && !children) {
+			return "This node does not have children and is the end of the word";
+		}
+		else {
+			return "Incorrect data entry/processing"; //this just shouldn't happen
+		}
+	}
 }
