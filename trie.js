636ae137595f947b114ed1997d9fc59d628d3a6f
nehakrishnakumar <nehakk.2014@gmail.com>
2020-04-28 12:38:22 -0400

    Remove unnecessary trie_node.js and merge with trie.js

--- trie.js
+++ trie.js
@@ -1,3 +1,12 @@
 //Trie data structure - for sake of autocomplete algorithm
 
+class TrieNode {
+	boolean children;
+	boolean endOfWord;
+
+	constructor (c, e) {
+		this.children = c;
+		this.endOfWord = e;
+	}
+}
 
